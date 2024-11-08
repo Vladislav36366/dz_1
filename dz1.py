0119a421c9ef303da3f299365756599d42355de1
Vladislav36366 <mihailo04@mail.ru>
2024-11-08 14:42:12 +0500

    Update dz1.py

--- dz1.py
+++ dz1.py
@@ -6,9 +6,8 @@ class House:
         if new_floor <= self.number_of_floors:
             for i in range(new_floor):
                 print(i+1)
-        else:
+        if new_floor > self.number_of_floors or new_floor < 0:
             print('Такого этажа нет')
-
 
 
 
@@ -17,3 +16,5 @@ h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
+h1.go_to(-4)
+h1.go_to(20)
